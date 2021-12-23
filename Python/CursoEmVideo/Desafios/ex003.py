8c04a54f9824289409739e076a623ea6058d8bc0
Isabelle Araújo <isabelletecn@gmail.com>
2021-12-23 18:10:06 -0300

    exercicios 1~2 | desafios 3
    Reorganização.
    Novos exercícios.

---
+++ Python/CursoEmVideo/Desafios/ex003.py
@@ -0,0 +1,6 @@
+#Crie um script Python que leia dois numeros e tente mostrar a soma entre eles
+
+a = input('Vamos somar! Digite um numero. ')    # a = int(input('Vamos somar! Digite um numero. '))
+b = input('Digite outro numero ')               # b = int(input('Digite outro numero '))
+soma = int(a) + int(b)                          # soma = a + b
+print('A soma destes números é {}'.format(soma))
